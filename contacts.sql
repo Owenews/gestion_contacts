df0c39b2eda49589010425d3a1aab98b95ff54b0
Owenews <115170215+Owenews@users.noreply.github.com>
2024-11-15 18:16:24 +0100

    contacts.sql updated
    - modification of nom to name and telephone to phone 
    - add some contact for a database filled in advance

--- contacts.sql
+++ contacts.sql
@@ -30,11 +30,21 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `telephone` varchar(15) NOT NULL,
+  `phone` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `name`, `email`, `phone`) VALUES
+(1, 'Iluobe', 'iluobe.owen@gmail.com', '0782789246'),
+(2, 'Owen', 'owenpromail@gmail.com', '0782789249'),
+(4, 'test', 'test@promail.com', '0782289249'),
+(5, 'Redo', 'rede.m@gmail.com', '0112347789');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
