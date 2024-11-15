fd025df88ea3fa1741ac281f46ae23bfe6548116
Owen <owenouiluobe@gmail.com>
2024-11-15 14:33:03 +0100

    rename the title of my Form and my Page Tab

--- add_contact.php
+++ add_contact.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ajouter un contact</title>
+    <title>Add a contact - Contact Manager</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <!-- Contact addition form -->
     <div class="container-fluid py-5">
-        <h2 class="mb-3">Ajouter un contact</h2>
+        <h2 class="mb-3">Add a contact</h2>
         <form method="post">
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
